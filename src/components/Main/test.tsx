ec3c23c4f08a937846b616dd7d1df8e7be2be6f8
Alan Gonçalves <vekio.soft@gmail.com>
2021-11-12 12:35:24 -0300

    Add RTL config and first test

---
+++ src/components/Main/test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react'
+
+import Main from '../Main'
+
+describe('Testa o componenete Main', () => {
+  it('should render the heading', () => {
+    const { container } = render(<Main />)
+    expect(
+      screen.getByRole('heading', { name: /React avançado/i })
+    ).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
