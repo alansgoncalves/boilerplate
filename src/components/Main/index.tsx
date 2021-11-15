d3317a5182a7d5cf6d636da29d2054f2d01a0323
Alan Gonçalves <vekio.soft@gmail.com>
2021-11-15 10:14:46 -0300

    test implementing

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import * as S from './styles'
+
 const Main = () => (
-  <div>
-    <h2>React avançado</h2>
-  </div>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React avançado escrito ao lado"
+    />
+    <S.Title>React avançado</S.Title>
+    <S.Description>
+      Typescript, ReactJs, NextJs, Styled components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Pessoa desenvolvedora de frente para uma tela com código"
+    />
+  </S.Wrapper>
 )
 
 export default Main
--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -11,4 +11,11 @@ describe('Testa o componente Main', () => {
 
     expect(container.firstChild).toMatchSnapshot()
   })
+  it('should render colors correctly', () => {
+    const { container } = render(<Main />)
+    expect(container.firstChild).toHaveStyle({
+      'background-color': '#06092b',
+      color: '#fff'
+    })
+  })
 })
