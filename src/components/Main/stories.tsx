747091d6f4c2aa2997572c6ed6ae13d539688f11
Alan Gonçalves <vekio.soft@gmail.com>
2021-12-09 09:38:56 -0300

    testing args in component Main

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -3,7 +3,13 @@ import Main from '.'
 
 export default {
   title: 'Main',
-  component: Main
+  component: Main,
+  args: {
+    title: 'Afrociberdelia',
+    description: 'Chico Science e Nação Zumbi'
+  }
 } as Meta
 
 export const Basic: Story = (args) => <Main {...args} />
+
+export const Vekio: Story = (args) => <Main {...args} />
