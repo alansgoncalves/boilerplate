254f4b22607d3df46d7272c98312d1c972021335
alansgoncalves <vekio.soft@gmail.com>
2023-12-05 18:25:21 -0300

    test: Initial jest tests

--- .lintsstagedrc.js
+++ .lintsstagedrc.js
@@ -1,7 +1,8 @@
 module.exports = {
     // Lint $ Preetfy TS and JS files
-    '*.{js, jsx, ts, tsx}': (filenames) => [
-        `prettier --write ${filenames.join(' ')}`
-        `npm run lint --fix . ${filenames.join(' --file')}`,
+    '*.{js, jsx, ts, tsx}': (filenames) => [ // Filenames is an array of all the files that are staged and match the glob.
+        `prettier --write ${filenames.join(' ')}` // Prettify
+        `npm run lint --fix . ${filenames.join(' --file')}`, // Lint and fix
+        `npm test -- --findRelatedTests ${filenames.join(' ')}` // Run tests related to the changed files
     ]
 }
--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'], //Ignore stories files from test coverage
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'], //Setup files to run before each test
   modulePaths: ['<rootDir>/src/'], //Define the path to import modules
-  tranform: {
+  transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
   } //Transform files to run the tests
 }
