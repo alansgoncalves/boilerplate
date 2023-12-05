676bec93eb46d7e3c658f9d3aee97bd46af4b999
alansgoncalves <vekio.soft@gmail.com>
2023-12-05 17:57:11 -0300

    build: update jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  testEnvironment: 'jsdom',
-  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-  collectCoverage: true,
+  testEnvironment: 'jsdom', //Environment to run the tests
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'], //Ignore node_modules and .next folders from test coverage
+  collectCoverage: true, //Collect coverage information from all files
   collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'], //Ignore stories files from test coverage
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'], //Setup files to run before each test
+  modulePaths: ['<rootDir>/src/'], //Define the path to import modules
+  tranform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
+  } //Transform files to run the tests
 }
