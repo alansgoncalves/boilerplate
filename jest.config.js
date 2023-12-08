6d6d8d678ed6caaf803a49c8af54772806fbc043
alansgoncalves <vekio.soft@gmail.com>
2023-12-08 18:16:39 -0300

    config: add files to ignore in collectCoverageFrom

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,14 @@ module.exports = {
   testEnvironment: 'jsdom', //Environment to run the tests
   testPathIgnorePatterns: ['/node_modules/', '/.next/'], //Ignore node_modules and .next folders from test coverage
   collectCoverage: true, //Collect coverage information from all files
-  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'], //Ignore stories files from test coverage
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/stories',
+    '!src/types/**',
+    '!src/styles/**',
+    '!src/stories/**',
+    '!src/components/Main/stories.tsx'
+  ], //Ignore stories files from test coverage
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'], //Setup files to run before each test
   modulePaths: ['<rootDir>/src/'], //Define the path to import modules
   transform: {
